e2aba304cc4e824d1ee7032cb589180785515b3b
Stefan Pratter <stefan@20c.com>
2016-08-16 20:07:33 +0000

    tests for timedplugin and dataplugin

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,11 +1,29 @@
 import unittest
+import time
 
 import vodka.plugins
+import vodka.data
+import vodka.storage
 import vodka
 
 @vodka.plugin.register('test')
 class PluginA(vodka.plugins.PluginBase):
     pass
+
+@vodka.plugin.register('timed_test')
+class TimedPlugin(vodka.plugins.TimedPlugin):
+    def init(self):
+        self.counter = 0
+
+    def work(self):
+        self.counter += 1
+
+@vodka.plugin.register('data_test')
+class DataPlugin(vodka.plugins.DataPlugin):
+    
+    def work(self):
+        data = {"data":[], "ts" : time.time()}
+        return super(DataPlugin, self).work(data)
 
 class TestPlugin(unittest.TestCase):
    
@@ -17,4 +35,49 @@ class TestPlugin(unittest.TestCase):
     def test_get_plugin_class(self):
         self.assertEqual(PluginA, vodka.plugins.get_plugin_class("test"))
         
+
+class TestTimedPlugin(unittest.TestCase):
+    
+    def test_run(self):
+        plugin = vodka.plugin.get_instance({
+            "type" : "timed_test",
+            "interval" : 0.01
+        })
+        vodka.start(thread_workers=[plugin])
+        time.sleep(0.051)
+
+        self.assertEqual(plugin.counter, 6)
+
+
+class TestDataPlugin(unittest.TestCase):
+    
+    def test_run(self):
+        vodka.data.data_types.instantiate_from_config(
+        [{
+            "type" : "data_test",
+            "handlers" : [
+                {
+                    "type" : "store",
+                    "container" : "list",
+                    "limit" : 10
+                }
+             ]
+        }]
+)
+ 
+        plugin = vodka.plugin.get_instance({
+            "type" : "data_test",
+            "interval" : 0.01,
+            "data" : "data_test"
+        })
+
+        vodka.start(thread_workers=[plugin])
+
+        time.sleep(0.3)
+
+        self.assertEqual(len(vodka.storage.get("data_test")), 10)
+        for item in vodka.storage.get("data_test"):
+            self.assertEqual("data" in item, True)
+            self.assertEqual("ts" in item, True)
+
 
