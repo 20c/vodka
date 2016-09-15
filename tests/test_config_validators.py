be0c4c970961381fdd260e97042e2c869e44b122
Stefan Pratter <stefan@20c.com>
2016-09-15 11:59:45 +0000

    test config validators

---
+++ tests/test_config_validators.py
@@ -0,0 +1,18 @@
+import unittest
+import vodka.config.validators
+
+class TestConfigValidators(unittest.TestCase):
+    
+    def test_path_validator(self):
+        b,d = vodka.config.validators.path(__file__)
+        self.assertEqual(b, True)
+    
+    def test_host_validator(self):
+        b,d = vodka.config.validators.host("host:1")
+        self.assertEqual(b, True)
+
+        b,d = vodka.config.validators.host("host")
+        self.assertEqual(b, False)
+
+        b,d = vodka.config.validators.host("host:b")
+        self.assertEqual(b, False)
